bb87b53748c1508f9e983dff3f109fd932528468
Huzaifa <huzaifa.bin_ahmer@yahoo.com>
2024-01-02 19:40:35 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,32 @@
+/*
+*****************************************************************************************************************************************************************
+*    ||CREDITOR||
+*    Title: Java pong game 🏓
+*    Author: Bro Code
+*    Date: 2020
+*    Availability: https://youtu.be/oLirZqJFKPE?si=lLfff5sW67HIcSaV
+*
+*****************************************************************************************************************************************************************
+Name: Huzaifa Bin Ahmer
+Date: Wednesday, December 28, 2022
+Title: Ping Pong Game
+||Description||
+This program is a 2-player controlled classic Pong game and grants control over the slider to each player.
+Player 1 controls the left-slider with "W" key to move up & "S" key for move down.
+Player 2 controls the right-slider with "UP Arrow" key to move up & "DOWN Arrow" key to move down.
+The speed of the ball gradually increases as players strike it back & forth till a either one of them misses and the opposing players earns a point.
+To win both players must try to reach 7 points first by striking the ball to the other side.
+******************************************************************************************************************************************************************
+*/
 
+// Public Class PongGame which runs the game
+public class Main {
+	
+	// Main Method
+	public static void main (String [] args) {
+		
+		// Instance of GameFrame Class
+		GameFrame frame = new GameFrame();
+		
+	} // End of Main Method
+} // End of Public Class
